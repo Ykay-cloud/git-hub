e5226494b429852734d625048001c365d0e47b2a
Ykay-cloud <yusifyushau4@gmail.com>
2025-04-14 16:46:37 +0000

    mysecondcommit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React eazworld
+          Learn React eazworld e-commerce app
         </a>
       </header>
     </div>
