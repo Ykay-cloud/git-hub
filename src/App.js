abfce51f8e0eb8865e490e27267f18b2d43fdc05
Ykay-cloud <yusifyushau4@gmail.com>
2025-04-14 17:18:50 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React eazworld e-commerce app by faisal 
+          Learn React eazworld e-commerce app by faisal yussif
         </a>
       </header>
     </div>
