db68d18c9edd1900d896a3633bcc61f09e1455e5
Ykay-cloud <yusifyushau4@gmail.com>
2025-04-14 17:16:58 +0000

    mycomponent

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React eazworld e-commerce app
+          Learn React eazworld e-commerce app by faisal 
         </a>
       </header>
     </div>
